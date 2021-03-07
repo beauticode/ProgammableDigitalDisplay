a50552c2fb452cc59f56c1aef240a035d605bcd2
Gary S <realbeauticode@gmail.com>
2021-03-06 22:21:07 -0800

    Program Mode fully functional

--- characters.h
+++ characters.h
@@ -19,21 +19,67 @@ Digits (Port D): For AND operations, leaves LED bit untouched (1)
 (Active low)
 */
 //-----------------------------
-#define digclr 0xb1 //Clears all digit bits in use
+#define digclr 0xb1 //Clears all digit bits in use (except LED and reserved pins)
 //-----------------------------
 
 
 /*
-Pins(Port B):
+Segments (Port B):
 7  6 5 4 3 2 1 0
 dp g f e d c b a
 */
 //-----------------------------
+#define ch_0 0x3f //10111110
 #define ch_1 0x06 //00000110
 #define ch_2 0x5b  //01011011
 #define ch_3 0x4f //01001111
+#define ch_4 0x66
+#define ch_5 0x6d
+#define ch_6 0x7d
+#define ch_7 0x07
+#define ch_8 0xff
+#define ch_9 0x67
+#define ch_a 0x5f
+#define ch_b 0x7c
+#define ch_c 0x39
+#define ch_d 0x5e
+#define ch_e 0x79
+#define ch_f 0x71
+#define ch_g 0x6f
+#define ch_h 0x74
+#define ch_i 0x30
+#define ch_j 0x1e
+#define ch_k 0x76 //kinda not rly supported. use h
+#define ch_l 0x38
+#define ch_m 0x54 //not rly supported
+#define ch_n 0x37
+#define ch_o 0x5c
+#define ch_p 0x73
+#define ch_q 0x67 //not rly supported. use 9 
+#define ch_r 0x77
+#define ch_s 0x6d //not rly supported. use 5
+#define ch_t 0x78
+#define ch_u 0x3e
+#define ch_v 0x3e //kinda not rly supported. use u
+#define ch_w 0x3e //not rly supported 
+#define ch_x 0x76 //not rly supported
+#define ch_y 0x7a
+#define ch_z 0x5b //not rly supported
 //-----------------------------
 
+/*
+Buttons (Port E):
+  7    6          5        4       3    2     1     0       
+[na] [na]     playstop progerase but_4 but_3 but_2 but_1 
+*/
+//-----------------------------
+#define but_1 0x01 
+#define but_2 0x02  
+#define but_3 0x04 
+#define but_4 0x08 
+#define but_progerase 0x10
+#define but_playstop 0x20
+//-----------------------------
 
 /*
 Systick reload value for 5ms delay between each digit cycle, used for multiplexing
--- main.c
+++ main.c
@@ -4,20 +4,47 @@
 //#include "PLL.h"
 void initGPIO(void); //Configures Port B (segment control) and D (digit & LED control) for digital output. Port A (button input) for digital input
 void initSystick(void); //Configures interrupts for quick periodic input state change checking.
-
+void incrementChar(int8_t digit, int8_t *currentChar); //Increments the currentChar array in main() when called, digits 0-3 
 
 int main(void) {
-	int8_t currentChar[4]={ch_2,ch_3,ch_1,0xff}; //Pins of current character for each digit, to be currently displayed. Pull definition from characters.h
+	int8_t currentChar[4]={ch_0,ch_1,ch_2,ch_3}; //Pins of current character for each digit, to be currently displayed. Pull definition from characters.h
 	int8_t currentCharPos=digset_1; // Digits 1-4, pull definition from characters.h
-	int8_t displaymode=0; //0=program mode, 1=display programmed mode
-	int8_t ticks=0; //Keeps track of systick countdowns every 5ms, useful for display mode when alternating between words
-	//PLL_Init();
+	int8_t programmode=1; //0=program mode, 1=display programmed mode
+	int8_t buttonsPrevVal[6]={0};
+	int32_t buttonDataRPrev; //Port A (button) data register previous value,  to check if it has changed.
+	int ticks=0; //Keeps track of systick countdowns every 5ms, useful for display mode when alternating between words
 	initGPIO();
 	initSystick();
-	/*GPIO_PORTD_DATA_R&=digclr_2;
-	GPIO_PORTD_DATA_R|=digset_2;//Debug line: Display 1 on digit 1
-	GPIO_PORTB_DATA_R|=ch_3; //Debug line continued*/
+	buttonDataRPrev=GPIO_PORTE_DATA_R;
 	while(1) {
+		if(GPIO_PORTE_DATA_R!=buttonDataRPrev) { //If any button state has changed
+			if((GPIO_PORTE_DATA_R&but_1)!=buttonsPrevVal[0]) { //If button 1 state has changed
+				if((GPIO_PORTE_DATA_R&but_1)==but_1) { //...and it's currently HIGH
+					incrementChar(0,currentChar);
+				}
+				buttonsPrevVal[0]=GPIO_PORTE_DATA_R&ch_1;
+			} //No else, just if to support multiple buons being pressed at once
+			if((GPIO_PORTE_DATA_R&but_2)!=buttonsPrevVal[1]) { //If button 1 state has changed
+				if((GPIO_PORTE_DATA_R&but_2)==but_2) { //...and it's currently HIGH
+					incrementChar(1,currentChar);
+				}
+				buttonsPrevVal[1]=GPIO_PORTE_DATA_R&but_2;
+			}
+			if((GPIO_PORTE_DATA_R&but_3)!=buttonsPrevVal[2]) { //If button 1 state has changed
+				if((GPIO_PORTE_DATA_R&but_3)==but_3) { //...and it's currently HIGH
+					incrementChar(2,currentChar);
+				}
+				buttonsPrevVal[2]=GPIO_PORTE_DATA_R&but_3;
+			}
+			if((GPIO_PORTE_DATA_R&but_4)!=buttonsPrevVal[3]) { //If button 1 state has changed
+				if((GPIO_PORTE_DATA_R&but_4)==but_4) { //...and it's currently HIGH
+					incrementChar(3,currentChar);
+				}
+				buttonsPrevVal[3]=GPIO_PORTE_DATA_R&but_3;
+			}			
+			buttonDataRPrev=GPIO_PORTE_DATA_R;
+		}
+		
 		if((NVIC_ST_CTRL_R & sysTickFlag)==sysTickFlag) { //If the flag is set (5ms has passed)
 			if((currentCharPos&(int8_t)digset_1)==(int8_t)digset_1) { //Change digit positionn
 				GPIO_PORTD_DATA_R&=digclr; //Clear current digit pins, but not all bc LED may be high
@@ -41,28 +68,30 @@ int main(void) {
 				GPIO_PORTB_DATA_R&=~GPIO_PORTB_DATA_R;
 				GPIO_PORTB_DATA_R|=currentChar[0];
 			}
-			if(!displaymode) { //are we in display mode?
-				
-			} else {
-				
-			}
 			GPIO_PORTD_DATA_R|=currentCharPos; //Set current digit high
 			ticks++;
+			if(programmode) { //are we in display mode or program mode?
+				
+			} else {
+				if(ticks==200) { //if 1000ms has passed, display next
+					
+				}
+			}
 		}
 	}
 	return 0;
 }
 
 void initGPIO(void) {
-	SYSCTL_RCGC2_R=0x0B; //Set 1011 for D, B, and A runmode clock enable
+	SYSCTL_RCGC2_R=0x1A; //Set 1011 for D, B, and E runmode clock enable
 	for(int8_t op=0,count=0;count<4;count++)
 		op+=1;
-	//GPIOA configuration (Inputs)
-	GPIO_PORTA_DIR_R&=~0xFC;
-	GPIO_PORTA_DEN_R|=0xFC;
-	GPIO_PORTA_AMSEL_R&=0x00;
-	GPIO_PORTA_AFSEL_R&=0x00;
-	GPIO_PORTA_PCTL_R&=~0xFC;
+	//GPIOE configuration (Inputs)
+	GPIO_PORTE_DIR_R&=~0x3F;
+	GPIO_PORTE_DEN_R|=0x3F;
+	GPIO_PORTE_AMSEL_R&=0x00;
+	GPIO_PORTE_AFSEL_R&=0x00;
+	GPIO_PORTE_PCTL_R&=~0x3F;
 	//GPIOB configuration (Segments)
 	GPIO_PORTB_DIR_R|=0xFF;
 	GPIO_PORTB_DEN_R|=0xFF;
@@ -84,7 +113,118 @@ void initSystick(void) {
 	NVIC_ST_CTRL_R|=0x05; //no interrupt, just checking time
 }
 
-/*void SysTick_Handler(void) {
-	
-}*/
-
+void incrementChar(int8_t digit, int8_t *currentChar) {
+	int8_t currentCharHex=currentChar[digit];
+	switch(currentCharHex) { //It's not as easy as doing currentCharHex[digit]++ because we need specific segment pins turned on
+		case ch_0:
+			currentChar[digit]=ch_1;
+		break;
+		case ch_1:
+			currentChar[digit]=ch_2;
+		break;
+		case ch_2:
+			currentChar[digit]=ch_3;
+		break;
+		case ch_3:
+			currentChar[digit]=ch_4;
+		break;
+		case ch_4:
+			currentChar[digit]=ch_5;
+		break;
+		case ch_5:
+			currentChar[digit]=ch_6;
+		break;
+		case ch_6:
+			currentChar[digit]=ch_7;
+		break;
+		case ch_7:
+			currentChar[digit]=ch_8;
+		break;
+		case (int8_t)ch_8:
+			currentChar[digit]=ch_9;
+		break;
+		case ch_9:
+			currentChar[digit]=ch_a;
+		break;
+		case ch_a:
+			currentChar[digit]=ch_b;
+		break;
+		case ch_b:
+			currentChar[digit]=ch_c;
+		break;
+		case ch_c:
+			currentChar[digit]=ch_d;
+		break;
+		case ch_d:
+			currentChar[digit]=ch_e;
+		break;
+		case ch_e:
+			currentChar[digit]=ch_f;
+		break;
+		case ch_f:
+			currentChar[digit]=ch_g;
+		break;
+		case ch_g:
+			currentChar[digit]=ch_h;
+		break;
+		case ch_h:
+			currentChar[digit]=ch_i;
+		break;
+		case ch_i:
+			currentChar[digit]=ch_j;
+		break;
+		case ch_j:
+			currentChar[digit]=ch_k;
+		break;
+		case ch_k:
+			currentChar[digit]=ch_l;
+		break;
+		case ch_l:
+			currentChar[digit]=ch_m;
+		break;
+		case ch_m:
+			currentChar[digit]=ch_n;
+		break;
+		case ch_n:
+			currentChar[digit]=ch_o;
+		break;
+		case ch_o:
+			currentChar[digit]=ch_p;
+		break;
+		case ch_p:
+			currentChar[digit]=ch_r;
+		break;
+		/*case ch_q:
+			currentChar[digit]=ch_r;
+		break;*/
+		case ch_r:
+			currentChar[digit]=ch_t;
+		break;
+		/*case ch_s:
+			currentChar[digit]=ch_t;
+		break;*/
+		case ch_t:
+			currentChar[digit]=ch_u;
+		break;
+		case ch_u:
+			currentChar[digit]=ch_y;
+		break;		
+		/*case ch_v:
+			currentChar[digit]=ch_w;
+		break;
+		case ch_w:
+			currentChar[digit]=ch_x;
+		break;
+		case ch_x:
+			currentChar[digit]=ch_y;
+		break;*/
+		case ch_y:
+			currentChar[digit]=0x00;
+		break;
+		case 0x00:
+				currentChar[digit]=ch_0;
+		/*case ch_z:
+			currentChar[digit]=ch_0;
+		break;*/
+	}
+}
